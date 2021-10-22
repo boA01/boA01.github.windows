aed7af6765df93ffc3d481c2571d1910c2477cec
boa01 <boA_999@163.com>
2021-10-22 17:32:51 +0800

    学习了描述符

---
+++ python基础/描述符.py
@@ -0,0 +1,44 @@
+'''
+描述符协议
+实现__set__(), __get__, __delete__()至少一个方法
+'''
+
+class Score():
+    def __init__(self, default):
+        self.name = default
+
+    def __set__(self, instance, value):
+        if 0<=value<=100:
+            instance.__dict__[self.name]=value
+        else:
+            raise ValueError("value error")
+    
+    def __get__(self, instance, owner):
+        return instance.__dict__[self.name]
+    
+    def __delete__(slef):
+        del self._score
+
+class Student():
+    math = Score("math")
+    chinese = Score("Chinese")
+    cs = Score("cs")
+
+    def __init__(self, *args):
+        self.name = args[0]
+        self.math = args[1]
+        self.chinese = args[2]
+        self.cs = args[3]
+
+    def __str__(self):
+        return f"name:{self.name}\
+            math:{self.math}\
+            chinese:{self.chinese}\
+            cs:{self.cs}"
+
+
+if __name__ == "__main__":
+    s1 = Student("xm", 66, 34, 45)
+    s2 = Student("xh", 69, 39, 49)
+    print(s1)
+    print(s2)
