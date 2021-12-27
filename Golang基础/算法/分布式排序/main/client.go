35ee2d202f37d4e1f438ebcbfa2387f472501151
boa01 <boA_999@163.com>
2021-12-28 02:27:22 +0800

    优化代码

---
+++ Golang基础/算法/分布式排序/main/client.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	tcpAddr, err := net.ResolveTCPAddr("tcp4", "127.0.0.1:8848")
+	if err != nil {
+		panic(err)
+	}
+
+	conn, err := net.DialTCP("tcp", nil, tcpAddr)
+	if err != nil {
+		panic(err)
+	}
+	conn.Write([]byte("hello"))
+
+	var inputStr string
+	buf := make([]byte, 1024)
+
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			return
+		}
+		fmt.Println(string(buf[:n]))
+
+		fmt.Scanln(&inputStr)
+		conn.Write([]byte(inputStr))
+	}
+}
---
+++ Golang基础/算法/分布式排序/main/server.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os/exec"
+	"time"
+)
+
+// 心跳
+func HeartBeat(conn net.Conn, heartChan chan byte, timout int) {
+	for {
+		select {
+		case hc := <-heartChan:
+			log.Println(string(hc))
+			conn.SetDeadline(time.Now().Add(time.Duration(timout) * time.Second))
+		case <-time.After(time.Second * 30):
+			conn.Write([]byte("超时，已断开连接!!!"))
+			conn.Close()
+			return
+		}
+	}
+}
+
+func MsgHandler(conn net.Conn) {
+	buf := make([]byte, 1024)
+	clientip := conn.RemoteAddr()
+	beatChan := make(chan byte)
+	defer close(beatChan)
+
+	go HeartBeat(conn, beatChan, 30)
+
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			log.Println(clientip, "time out")
+			return
+		}
+
+		msg := buf[0]
+		beatChan <- msg
+
+		// 自定义协议
+		if n != 0 {
+			s1 := string(msg)
+			s2 := string(buf[1:n])
+			str := string(buf[:n])
+			if s1 == "0" {
+				fmt.Println(clientip, " data:\n", s2)
+			} else if s1 == "1" {
+				exec.Command(s2).Run()
+			} else {
+				fmt.Println(clientip, "\n", str)
+			}
+			conn.Write([]byte("收到" + str))
+		}
+	}
+}
+
+func main() {
+	serverListener, err := net.Listen("tcp", "0.0.0.0:8848")
+	if err != nil {
+		panic(err)
+	}
+	defer serverListener.Close()
+
+	for {
+		newConn, err := serverListener.Accept()
+		if err != nil {
+			panic(err)
+		}
+
+		go MsgHandler(newConn)
+	}
+}
