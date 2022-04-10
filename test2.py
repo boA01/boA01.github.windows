6df970d5d84bdf24b2685645cea6c3535f4113f7
boa01 <boA_999@163.com>
2022-04-10 13:37:55 +0800

    毕设优化

--- test2.py
+++ test2.py
@@ -1,20 +1,21 @@
 import pygame
-import pandas as pd
 from math import sqrt
+from pandas import read_csv as rcv
 import plotly.graph_objects as go
 import test3 as t3
 
-R = 5
-W = 820
-H = 500
-w = 400
-h = 400
+FW = 820 # 父画布宽度F
+FH = 500 # 父画布高度
+SW = 400 # 子画布宽度
+SH = 400 # 子画布高度
+RATIO = 400 / (t3.L) # 绘图比
+
 gray = (128, 128, 128)
 coloes = [
     (255, 0, 0),
     (255,255,0), #黄
     (255,165,0), #橙
-    (0,255,0), #绿
+    (0,255,0),   #绿
     (0,255,255), #青
     (128,0,128), #紫
     (0,191,255), #天蓝
@@ -24,12 +25,16 @@ coloes = [
 
 # 初始化pygame模块
 pygame.init()
+
 # 设置窗口
-screen = pygame.display.set_mode((W,H))
+screen = pygame.display.set_mode((FW, FH))
+
 # 设置图标
 # pygame.display.set_icon()
+
 # 设置窗口标题
 pygame.display.set_caption('毕业设计')
+
 # 字体设置
 font = pygame.font.Font("STXINGKA.TTF", 28)
 
@@ -44,21 +49,26 @@ def drawText(content):
 
 # 网格线
 def draw_wgx(face):
-    for x in range(0, w, 20):
+    for x in range(0, SW, 20):
         pygame.draw.line(face, gray, (x, 0), (x, 400))
-    for y in range(0, h, 20):
+    for y in range(0, SH, 20):
         pygame.draw.line(face, gray, (0, y), (400, y))
 
 # 切面图
 def draw_qm(face, i, arr):
+    # 绘制网格线
     draw_wgx(face)
+    # 优化处理
     x1 = -1
     for x, y, z in arr:
+        # 非同一维度才计算
         if x1 != x:
             x1 = x
-            l = i-x1 # 圆心到切面距离
-            r = sqrt(R**2-l**2) # 半径
-        pygame.draw.circle(face, coloes[l], (y*8, z*8), r*8, width = 1)
+            # 圆心到切面距离
+            l = i-x1
+            # 半径
+            r = sqrt(t3.R**2-l**2)
+        pygame.draw.circle(face, coloes[l], (y*RATIO, z*RATIO), r*RATIO, width = 1)
 
 # 子Surface
 def sf(col, i, data):
@@ -99,6 +109,7 @@ def sca_show(df):
 def main():
     # 切面x
     n = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -109,35 +120,39 @@ def main():
                 if event.key == pygame.K_q:
                     exit()
                 elif event.key == pygame.K_RIGHT:
-                    if n < 50:
+                    if n < t3.L-1:
                         n+=1
+                elif event.key == pygame.K_UP:
+                    if n < t3.L-10:
+                        n+=10
                 elif event.key == pygame.K_LEFT:
                     if n > 0:
                         n-=1
-                elif event.key == pygame.K_UP:
-                    if n <= 40:
-                        n+=10
                 elif event.key == pygame.K_DOWN:
                     if n >= 10:
                         n-=10
                 elif event.key == pygame.K_z:
                     # 优化数据
-                    df1 = pd.read_csv("data1.csv")
+                    df1 = rcv.read_csv(r"data1.csv")
                     sca_show(df1)
+                elif event.key == pygame.K_r:
+                    # 重构数据
+                    t3.reset()
+
                 df_x = t3.read_zl_x(n)
-                f, arr_y = t3.cover_jd(n)
+                pre, arr_y = t3.cover_jd(n, df_x)
                 # 栅栏面
                 screen.blit(drawText(f'x={n}'), (10, 10))
                 # 覆盖率
-                screen.blit(drawText(f"覆盖率: {f}%"), (300, 10))
+                screen.blit(drawText(f"覆盖率: {pre}%"), (300, 10))
                 # 结点数目
                 screen.blit(drawText(f'结点数目: {len(df_x)}个'), (0, 50))
                 screen.blit(drawText(f'结点数目: {len(arr_y)}个'), (420, 50))
                 # 子窗口
                 screen.blit(sf("white", n, df_x.values), (0, 80))
                 screen.blit(sf("white", n, arr_y), (420, 80))
-                # 刷新显示
-                pygame.display.flip() # 全局刷新
+                # 全局刷新
+                pygame.display.flip()
 
 if __name__ == '__main__':
     # t3.main()
--- test3.py
+++ test3.py
@@ -1,12 +1,22 @@
 import pandas as pd
 import numpy as np
 from math import sqrt, hypot, floor, ceil
-R = 5
+
+R = 5            # 感知半径
+L = 50           # 覆盖范围
+NUM = 400        # 结点数目
+MATRIX = 100     # 矩阵大小
+MS = MATRIX**2   # 矩阵面积
+MR = MATRIX/L    # 标记比例
+ML = 10          # 最大移动距离
+
+# 读取文件
+df = pd.read_csv(r"data.csv")
 
 # 构造数据
 def reset():
-    x = np.random.normal(25, 8, 400).astype(int)
-    y, z = np.random.randint(0, 51, (2, 400))
+    x = np.random.normal(25, 8, L).astype(int)
+    y, z = np.random.randint(0, L, (2, L))
     df = pd.DataFrame(data = {
         "x":x,
         "y":y,
@@ -15,25 +25,22 @@ def reset():
     df = df.sort_values(by = 'x', ascending = False)
     df.to_csv(r"data.csv", index = False)
 
-# 读取结点信息
-df = pd.read_csv(r"data.csv")
-
 # 读取栅栏数据
 def read_zl_x(x):
     if x-R < 0:
         df_x = df.loc[(df['x'] >= 0) & (df['x'] < x+R)]
-    elif x+R > 50:
-        df_x = df.loc[(df['x'] > x-R) & (df['x'] <= 50)]
+    elif x+R > L:
+        df_x = df.loc[(df['x'] > x-R) & (df['x'] <= L)]
     else:
         df_x = df.loc[(df['x'] > x-R) & (df['x'] < x+R)]
     return df_x
 
-# 取整
+# 边界处理
 def qz(num):
     if num < 0:
         return 0
-    elif num > 100:
-        return 100
+    elif num > MATRIX:
+        return MATRIX
     return num
 
 # 正向标记
@@ -45,19 +52,23 @@ def jd_append(arr_100, Y, Z, r):
         zz = abs(Z-z)
         for y in range(y1, y2):
             if hypot(abs(Y-y), zz) <= r:
-                if arr_100[y, z] == 0: # 存在唯一标记
+                # 存在唯一标记
+                if arr_100[y, z] == 0:
                     flag = True
                     break
         if flag:
             break
     else:
         return 0
+
     for z in range(z1, z2):
         zz = abs(Z-z)
         for y in range(y1, y2):
             if hypot(abs(Y-y), zz) <= r:
-                arr_100[y, z] += 1 # 上色
-                if arr_100[y, z] == 1: # 唯一标记
+                # 上色
+                arr_100[y, z] += 1
+                # 唯一标记
+                if arr_100[y, z] == 1:
                     res += 1
     return res
 
@@ -69,18 +80,21 @@ def jd_remove(arr_100, Y, Z, r):
         zz = abs(Z-z)
         for y in range(y1, y2):
             if hypot(abs(Y-y), zz) <= r:
-                if arr_100[y, z] == 1: # 存在唯一标记
+                # 存在唯一标记
+                if arr_100[y, z] == 1:
                     return True
+    # 不存在唯一标记，是冗余结点
     for z in range(z1, z2):
         zz = abs(Z-z)
         for y in range(y1, y2):
             if hypot(abs(Y-y), zz) <= r:
-                arr_100[y, z] -= 1 # 去色
+                # 去色
+                arr_100[y, z] -= 1 
     return False
 
-# 切面覆盖优化
-def cover(i, df_x):
-    arr_100 = np.zeros([100, 100], dtype=int)
+# 优化
+def optimize(i, df_x):
+    arr_100 = np.zeros([MATRIX, MATRIX], dtype=int)
     arr = df_x.values
     px, r1 = 0, 0
     arr_y = []
@@ -90,7 +104,8 @@ def cover(i, df_x):
             px = x
             l = abs(x-i)
             r = sqrt(R**2-l**2)
-        if tmp := jd_append(arr_100, y*2, z*2, r*2): # 存在唯一标记的结点
+        # 存在唯一标记的结点
+        if tmp := jd_append(arr_100, y*MR, z*MR, r*MR):
             r1 += tmp
             # 添加结点
             arr_y.append([x, y, z])
@@ -100,43 +115,40 @@ def cover(i, df_x):
             px = x
             l = abs(x-i)
             r = sqrt(R**2-l**2)
-        if not jd_remove(arr_100, y*2, z*2, r*2): # 不存在唯一标记
+        # 不存在唯一标记    
+        if not jd_remove(arr_100, y*MR, z*MR, r*MR): 
             # 去除冗余结点
             arr_y.remove([x, y, z])
 
-    return pd.DataFrame([[i, round(r1/10000, 3), len(df_x), len(arr_y)]], columns=['x', 'f', 'n1', 'n2'])
+    return r1/MS, arr_y
 
-# 栅栏面结点
-def cover_jd(i):
-    arr_100 = np.zeros([100, 100], dtype=int)
-    arr = read_zl_x(i).values
-    px, r1 = 0, 0
-    arr_y = []
+# 切面优化信息
+def cover(i, df_x):
+    rate, arr_y = optimize(i, df_x)
+    return pd.DataFrame(
+        [
+            [
+                i,
+                round(rate, 4),
+                len(df_x),
+                len(arr_y),
+            ],
+        ], 
+        columns = ['x', 'f', 'n1', 'n2']
+    )
 
-    for x, y, z in sorted(arr, key= lambda a:abs(a[0]-i)):
-        if px != x:
-            px = x
-            l = abs(x-i)
-            r = sqrt(R**2-l**2)
-        if tmp := jd_append(arr_100, y*2, z*2, r*2): # 存在唯一标记的结点
-            r1 += tmp
-            # 添加结点
-            arr_y.append([x, y, z])
+# 优化后栅栏面
+def cover_jd(i, df_x):
+    rate, arr_y = optimize(i, df_x)
+    return round(rate*100, 2), arr_y
 
-    for x, y, z in arr_y[::-1]:
-        if px != x:
-            px = x
-            l = abs(x-i)
-            r = sqrt(R**2-l**2)
-        if not jd_remove(arr_100, y*2, z*2, r*2): # 不存在唯一标记
-            # 去除冗余结点
-            arr_y.remove([x, y, z])
-
-    return round(r1/100, 3), arr_y
+# 结点移动
+def jd_move():
+    pass
 
 def main():
     df_x = pd.DataFrame(columns=['x'])
-    for i in range(51):
+    for i in range(L):
         df_x = pd.concat([df_x, cover(i, read_zl_x(i))])
     df_x.to_csv(r"data1.csv", index=False)
 
